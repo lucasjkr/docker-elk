2d2695b8c8fbbf4b82ae9eabe126a983f0a267a9
lucasjkr <lucas@lucaskrupinski.com>
2018-07-30 23:16:46 -0400

    update Elasticsearch Dockerfile
    templates and install script are copied during container build, but need to be inserted manually after the build.

---
+++ elasticsearch/templates/install-templates.sh
@@ -0,0 +1,2 @@
+curl -XPUT -H 'Content-Type: application/json' http://elasticsearch:9200/_template/winlogbeat-6.3.2 -d@winlogbeat.template.json
+curl -XPUT -H 'Content-Type: application/json' http://elasticsearch:9200/_template/metricbeat-6.3.2 -d@metricbeat.template.json
