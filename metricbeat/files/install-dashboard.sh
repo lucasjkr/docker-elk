c27a1462a9778fb9c747d2d50f498c464715aaa0
Lucas Krupinski <lucasjkr@me.com>
2018-07-31 22:48:33 +0000

    Remove metricbeat dashboard installation from Dockerfile, just copy the install script and give permissions so we can log in and install the templates once

---
+++ metricbeat/files/install-dashboard.sh
@@ -0,0 +1,7 @@
+metricbeat setup -e  \
+  -E output.logstash.enabled=false\
+  -E output.elasticsearch.hosts=['elasticsearch:9200']\
+  -E output.elasticsearch.username=metricbeat_internal\
+  -E output.elasticsearch.password=YOUR_PASSWORD\
+  -E setup.kibana.host=kibana:5601
+
