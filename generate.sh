92145ed7c8143f5673a55d6879d420d4350620d0
lucasjkr <lucas@lucaskrupinski.com>
2018-08-03 13:07:22 -0400

    Update generate.sh with NGinx SSL key

--- generate.sh
+++ generate.sh
@@ -10,9 +10,8 @@ openssl req -x509 -batch -nodes -days 3650\
   -out opt/logstash-forwarder.crt\
   -subj "/"
 
-openssl req -x509 -batch -nodes -days 3650\
-  -newkey rsa:4096\
-  -keyout opt/nginx-proxy.key\
-  -out opt/nginx-proxy.crt\
-  -subj "/"
-
+openssl req -x509 -newkey rsa:4086 \
+  -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=ElkStack" \
+  -keyout "opt/nginx-key.pem" \
+  -out "opt/nginx-cert.pem" \
+  -days 3650 -nodes -sha256
