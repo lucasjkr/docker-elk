68c7111960e01f363aadd4b03f46d747107d32fe
Lucas Krupinski <lucaskrupinski@me.com>
2018-08-04 12:31:52 -0400

    add metricbeat dashboards to Kibana

--- metricbeat/setup/install-dashboard.sh
+++ metricbeat/setup/install-dashboard.sh
@@ -1,1 +1,1 @@
-metricbeat setup -e\ -E output.logstash.enabled=false\ -E output.elasticsearch.hosts=['elasticsearch:9200']\ -E output.elasticsearch.username=metricbeat_internal\ -E output.elasticsearch.password=YOUR_PASSWORD\ -E setup.kibana.host=kibana:5601
+metricbeat setup -E output.logstash.enabled=false\ -E output.elasticsearch.hosts=['elasticsearch:9200']\ -E output.elasticsearch.username=metricbeat_internal\ -E output.elasticsearch.password=YOUR_PASSWORD\ -E setup.kibana.host=kibana:5601
