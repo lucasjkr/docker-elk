200934c9a37d4a41b7f21c1f568ace37b23a08b2
Lucas Krupinski <lucaskrupinski@me.com>
2018-08-08 15:38:47 -0400

    Delete separate Build file for the 411 container
    Two containers built from same image, just run different scripts at the end.

--- 411-web/cron-entrypoint.sh
+++ 411-web/cron-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while [ true ]
 do
-    /usr/bin/php /var/www/411/bin/cron.php
+    /usr/local/bin/php /var/www/411/bin/cron.php
     echo "cron.php completed"
-    /usr/bin/php /var/www/411/bin/worker.php
+    /usr/local/bin/php /var/www/411/bin/worker.php
     echo "worker.php completed"
     sleep 60
 done
--- 411-web/setup.sh
+++
@@ -1,4 +0,0 @@
-php -f bin/migration.php
-php -f bin/create_site.php
-php -f bin/create_user.php
-php
