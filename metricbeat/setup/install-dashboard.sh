66b05781a8484122556ea4dd64004b8d76967716
lucasjkr <lucas@lucaskrupinski.com>
2018-08-09 11:22:44 -0400

    remove dashboard install Bash script from MB
    this is now in the installation file

--- metricbeat/setup/install-dashboard.sh
+++
@@ -1,1 +0,0 @@
-metricbeat setup -E output.logstash.enabled=false\ -E output.elasticsearch.hosts=['elasticsearch:9200']\ -E output.elasticsearch.username=metricbeat_internal\ -E output.elasticsearch.password=YOUR_PASSWORD\ -E setup.kibana.host=kibana:5601
