4ea57cc5564f8165e31deb08970809502adb04a1
lucasjkr <lucas@lucaskrupinski.com>
2018-08-26 15:50:03 -0400

    Cleanup directories
    Moved the 411 and Handesk SQL files to SQL directories inside each of their projects, so that the MySQL build directory can be deleted.
    
    Renamed 411-web directory to 411

--- mysql/sql/00_create_empty_databases.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE fouroneone;
-
-CREATE USER 'fouroneone'@'%' IDENTIFIED BY 'fouroneone';
-GRANT ALL PRIVILEGES ON fouroneone.* TO 'fouroneone'@'%';
-
-CREATE DATABASE handesk;
-
-CREATE USER 'handesk'@'%' IDENTIFIED BY 'handesk';
-GRANT ALL PRIVILEGES ON handesk.* TO 'handesk'@'%'
