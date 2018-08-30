135a227cf06f689f0d020a53bf0869d452f639cc
lucasjkr <lucas@lucaskrupinski.com>
2018-08-30 12:38:18 -0400

    Elasticsearch does not need SSL keys, can delete the placeholders and the script that generates them

--- generate.sh
+++ generate.sh
@@ -1,11 +1,5 @@
 openssl req -x509 -batch -nodes -days 3650\
   -newkey rsa:4096\
-  -keyout opt/elasticsearch.key\
-  -out opt/elasticsearch.crt\
-  -subj "/"
-
-openssl req -x509 -batch -nodes -days 3650\
-  -newkey rsa:4096\
   -keyout opt/logstash-forwarder.key\
   -out opt/logstash-forwarder.crt\
   -subj "/"
